3a2a39ae27dbfdfbc252e161abf978a078deb935
Daniel Mitre <dbgmitre@gmail.com>
2023-12-09 21:49:11 +0000

    Add day 9 solutions

---
+++ 2023/day_9/part_2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import re
+
+from sys import stdin
+
+readings = [[int(r) for r in line.split()] for line in stdin.readlines()]
+
+largest = max(map(len, readings))
+layers = [[0 for _ in range(largest)] for _ in range(largest)]
+
+ans = 0
+for reads in readings:
+    size = len(reads)
+    for i in range(size):
+        layers[0][i] = reads[i]
+        for level in range(1, i + 1):
+            layers[level][i] = layers[level - 1][i] - layers[level - 1][i - 1]
+    value = 0
+    for i in range(size - 1, 0, -1):
+        value = layers[i][i] - value
+    ans += reads[0] - value
+
+print(ans)
