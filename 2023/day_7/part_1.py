fefdd0dbcf5e10e3f912cf3d8b6659ef732ca972
Daniel Mitre <dbgmitre@gmail.com>
2023-12-09 22:40:47 +0000

    refactor: Simplify day 7 solutions

--- 2023/day_7/part_1.py
+++ 2023/day_7/part_1.py
@@ -2,19 +2,11 @@
 # -*- coding: utf-8 -*-
 
 from collections import Counter
-from functools import cmp_to_key
 from sys import stdin
 
-def cmp(left, right):
-    card = 'AKQJT98765432'
-    hand_l, _, counter_l = left
-    hand_r, _, counter_r = right
+def cmp(cards):
+    order = 'AKQJT98765432'[::-1]
+    hand, _ = cards
+    return sorted(Counter(hand).values(), reverse=True), list(map(order.index, hand))
 
-    if len(counter_l) == len(counter_r):
-        if counter_r[0][1] == counter_l[0][1]:
-            return -1 if list(map(card.index, hand_l)) < list(map(card.index, hand_r)) else 1
-        return counter_r[0][1] - counter_l[0][1]
-    return len(counter_l) - len(counter_r)
-
-print(sum(bid * i for i, (_, bid, _) in enumerate(sorted(((hand, int(bid), Counter(hand).most_common()) for (hand, bid) in map(str.split, stdin.readlines())), key=cmp_to_key(cmp), reverse=True), 1)))
-
+print(sum(int(bid) * i for i, (_, bid) in enumerate(sorted(map(str.split, stdin.readlines()), key=cmp), 1)))
--- 2023/day_7/part_2.py
+++ 2023/day_7/part_2.py
@@ -2,23 +2,15 @@
 # -*- coding: utf-8 -*-
 
 from collections import Counter
-from functools import cmp_to_key
 from sys import stdin
 
-def cmp(left, right):
-    card = 'AKQT98765432J'
-    hand_l, _, counter_l = left
-    hand_r, _, counter_r = right
-    joker_l, joker_r = counter_l['J'], counter_r['J']
-    counter_r -= Counter({'J': joker_r})
-    counter_r += Counter({counter_r.most_common()[0][0] if len(counter_r.most_common()) else 'J': joker_r})
-    counter_l -= Counter({'J': joker_l})
-    counter_l += Counter({counter_l.most_common()[0][0] if len(counter_l.most_common()) else 'J': joker_l})
-    
-    if len(counter_l) == len(counter_r):
-        if counter_r.most_common()[0][1] == counter_l.most_common()[0][1]:
-            return -1 if list(map(card.index, hand_l)) < list(map(card.index, hand_r)) else 1
-        return counter_r.most_common()[0][1] - counter_l.most_common()[0][1]
-    return len(counter_l) - len(counter_r)
+def cmp(cards):
+    order = 'AKQT98765432J'[::-1]
+    hand, _ = cards
+    c = Counter(hand)
+    jokers = c.pop('J', 0)
+    freq = sorted(c.values() or [0], reverse=True)
+    freq[0] += jokers
+    return freq, list(map(order.index, hand))
 
-print(sum(bid * i for i, (_, bid, _) in enumerate(sorted(((hand, int(bid), Counter(hand)) for (hand, bid) in map(str.split, stdin.readlines())), key=cmp_to_key(cmp), reverse=True), 1)))
+print(sum(int(bid) * i for i, (_, bid) in enumerate(sorted(map(str.split, stdin.readlines()), key=cmp), 1)))
